aea2070d4b22f6330b85a5af5a1b3fa1cd6d3cea
MrMWebDev <tihanymario@gmail.com>
2023-10-02 10:20:50 +0100

    exercise 2

--- myApp.js
+++ myApp.js
@@ -1,5 +1,23 @@
 require('dotenv').config();
-const mongoose = require("mongoose").config();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//#1
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, useUnifiedTopology: true 
+},
+() => {
+  console.log("DB connection successful!!!")
+}
+);
+
+//#2
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
 
 let Person;
 
